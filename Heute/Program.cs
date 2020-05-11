640c4af3380655c41849a0d4ed64d424535f2681
Ruben Vitt <dev@rubeen.dev>
2020-05-11 13:47:39 +0200

    init(project)

---
+++ Heute/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Woche
+{
+    class Heute
+    {
+        static void Main(string[] args)
+        {
+            Laune soGehtEsMir = new Laune() { zustand = LaunenZustand.langeweile };
+            Console.Write(soGehtEsMir.ToString());
+        }
+    }
+
+    class Laune
+    {
+        public LaunenZustand zustand = new LaunenZustand();
+    }
+
+    enum LaunenZustand
+    {
+        gut,
+        langeweile
+    }
+}
